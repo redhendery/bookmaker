3731d0d1d2efc630b5b5094f4191de70e273aa85
redhendery <redhendery@gmail.com>
2020-08-28 17:14:11 -0700

    add renderer concern to API controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < Api::V1::BaseController
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      return render json: @user, status: :created
+      return render_object(@user, :created)
     end
 
-    render json: @user.errors, status: :unprocessable_entity
+    render_errors(@user.errors)
   end
 
   private
